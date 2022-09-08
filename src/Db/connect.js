78612207de9fc9c5ea660320f61af2bb4f9ea3a8
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-08 14:54:10 +0600

    added

---
+++ src/Db/connect.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_LOCAL).then(()=>{
+
+    console.log('Database connect successfully.');
+
+}).catch((error)=>{
+
+    console.log(error);
+
+})
