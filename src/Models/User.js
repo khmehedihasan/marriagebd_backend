3024b293154873f951a79f8b0aa0124d68cce698
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-12 18:31:32 +0600

    added

---
+++ src/Models/User.js
@@ -0,0 +1,175 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        reqire:true,
+        unique:true 
+    },
+    candidateName:{
+        type:String,
+        require:true
+    },
+    birthDate:{
+        type:String,
+        require:true
+    },
+    religion:{
+        type:String,
+        require:true,
+    },
+    maritalStatus:{
+        type:String,
+        require:true
+    },
+    nationality:{
+        type:String,
+        require:true
+    },
+    gender:{
+        type:String,
+        require:true
+    },
+    educationDetails:String,
+    workingSector:{
+        type:String,
+        require:true
+    },
+    educationQualification:{
+        type:String,
+        require:true
+    },
+    professionDetails:{
+        type:String,
+        require:true 
+    },
+    profession:{
+        type:String,
+        require:true 
+    },
+    incame:{
+        type:String,
+        require:true 
+    },
+    homeDicision:{
+        type:String,
+        require:true 
+    },
+    fatherStatus:{
+        type:String,
+        require:true 
+    },
+    familyDetails:String,
+    livingIn:{
+        type:String,
+        require:true 
+    },
+    fatherOccupation:String,
+    motherStatus:{
+        type:String,
+        require:true 
+    },
+    motherOccupation:String,
+    livingCity:{
+        type:String,
+        require:true 
+    },
+    marriedBrother:{
+        type:String,
+        require:true 
+    },
+    unMarriedBrother:{
+        type:String,
+        require:true  
+    },
+    marriedSister:{
+        type:String,
+        require:true  
+    },
+    unMarriedSister:{
+        type:String,
+        require:true 
+    },
+    physicalDetails:String,
+    weight:{
+        type:String,
+        require:true 
+    },
+    height:{
+        type:String,
+        requite:true 
+    },
+    blood:String,
+    bodyType:{
+        type:String,
+        require:true 
+    },
+    complexion:{
+        type:String,
+        require:true 
+    },
+    smoke:{
+        type:String,
+        requite:true 
+    },
+    religiousValue:{
+        type:String,
+        require:true 
+    },
+    familyVale:{
+        type:String,
+        require:true 
+    },
+    diet:{
+        type:String,
+        require:true 
+    },
+    aboutSelf:{
+        type:String,
+        requite:true,
+    },
+    verificationCode:String,
+    token:{
+        type:String,
+        require:true 
+    },
+    password:{
+        type:String,
+        require:true 
+    },
+    date:{
+        type:Date,
+        default: ()=> { return new Date() }
+    }
+
+    
+});
+
+
+userSchema.pre('save', async function(next){
+    this.password = await bcrypt.hash(this.password, parseInt(process.env.BCRYPT_LOOP));
+    next();
+});
+
+userSchema.methods.createToken = async function(){
+    try{
+         this.token = await jwt.sign({_id:this.id}, process.env.JWT_SEC, {expiresIn:process.env.JWT_EXP});
+    }catch(error){
+        console.log(error);
+    }
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
