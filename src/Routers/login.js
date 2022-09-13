14d0887257b229636f4c9d9b90a45ec65db8b533
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-13 17:55:33 +0600

    update

--- src/Routers/login.js
+++ src/Routers/login.js
@@ -2,6 +2,10 @@ const express = require('express');
 const login = require('../Controllers/login');
 const router = express.Router();
 
+router.post('/',login.loginUser);
 router.post('/forgotPassword',login.sendCode);
+router.post('/resetpassword/:id',login.reSetPassword);
+router.post('/varifyCode/:id',login.varifyCode);
+router.get('/resend/:id',login.resendCode);
 
 module.exports = router;
