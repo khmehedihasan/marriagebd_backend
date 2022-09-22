7a34add623fb5c16d085a4c1c6bd5c7e467adcda
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-22 18:44:57 +0600

    added

---
+++ src/Routers/chat.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const chat = require('../controllers/chat');
+
+router.post('/', chat.sendChat);
+router.get('/', chat.getMessage);
+router.get('/friends', chat.getFrends);
+
+module.exports = router;
