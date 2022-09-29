20e702d46725254108186517c5109e5ac2f68804
khmehedihasan <mehedihasan47341@gmail.com>
2022-09-29 18:47:33 +0600

    added

---
+++ src/Routers/payment.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const payment = require('../Controllers/payment');
+
+router.post('/:id', payment.addPayment);
+router.get('/', payment.getAllPayment);
+router.get('/search', payment.searchPayment);
+router.get('/:id', payment.getAllPaymentById);
+router.delete('/:id', payment.deletePayment);
+
+
+module.exports = router;
